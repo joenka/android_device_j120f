114b483a8ec7a07a11ddf389ea6b0ed59dc8310c
joenka <peter.geraskin@gmail.com>
2016-05-19 20:15:45 +0300

    Delete cm.mk

--- cm.mk
+++
@@ -1,6 +0,0 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, device/samsung/j120f/full_j120f.mk)
-
-PRODUCT_NAME := cm_j120f
